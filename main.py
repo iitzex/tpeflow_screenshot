e647916a7d3084e35b5368473489b15e89a102a3
iitzex <iitzex@gmail.com>
2019-04-02 12:10:25 +0800

    update for loop

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import asyncio
 import time
+import os
 
 from pyppeteer import launch
 
+SCREENSHOT = 'screenshot'
 
-async def main():
+async def capture():
     browser = await launch()
     page = await browser.newPage()
+    await page.setViewport({'width': 850, 'height': 600})
     addr = 'https://tpeflow.herokuapp.com/'
     ts = int(time.time())
 
     await page.goto(addr)
-    await page.screenshot({'path': f'tpeflow2_{ts}.png'})
+    await page.screenshot({'path': f'{SCREENSHOT}/tpeflow2_{ts}.png'})
     await browser.close()
 
-asyncio.get_event_loop().run_until_complete(main())
+
+if __name__ == "__main__":
+    if not os.path.isdir(SCREENSHOT):
+        os.mkdir(SCREENSHOT)
+
+    while(True):
+        asyncio.get_event_loop().run_until_complete(capture())
+        time.sleep(300)
